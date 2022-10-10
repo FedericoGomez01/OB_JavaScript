c4450c54b469db29bb998e68c3992555932328d2
Federico Gomez <fedegomez.pg@gmail.com>
2022-10-10 17:32:47 -0300

    Comienzo del pryecto

---
+++ mi-primer-proyecto-de-node/index.js
@@ -0,0 +1,10 @@
+// Este es el mensaje de bienvenida 
+console.log("Esta es la puerta de entrada al proyecto");
+
+// Esta es la parte intermedia del proyecto
+// Realizamos chequeos
+// Por ultimo usamos la linea de saludo.
+
+
+// Este va a ser el menaje de despedida
+console.log("Adios");
